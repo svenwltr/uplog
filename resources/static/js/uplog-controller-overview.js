797885874169c2fa03758a7e8509915b2455ca16
Sven Walter <sven@wltr.eu>
2015-04-17 15:57:13 +0200

    Added overview scores.

--- resources/static/js/uplog-controller-overview.js
+++ resources/static/js/uplog-controller-overview.js
@@ -1,5 +1,23 @@
 
 app.controller("OverviewController", function($scope, $http, $interval) {
+	$scope.$on('recordsUpdate', function(e, records) {
 
+		var currID;
+
+		records.forEach(function(record, i) {
+			if(record.active) {
+				currID = i;
+			}
+		});	
+
+		$scope.scores = {
+			best: records[records.length-1],
+			next: records[currID+1],
+			curr: records[currID],
+			prev: records[currID-1],
+			worst: records[0],
+		};
+
+	});
 
 });
