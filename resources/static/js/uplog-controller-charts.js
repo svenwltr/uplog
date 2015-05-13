7c5e4a5acdb556ca90312bef3c17fb9cd784f457
Sven Walter <sven@wltr.eu>
2015-05-13 14:31:40 +0200

    Added probability density chart.

--- resources/static/js/uplog-controller-charts.js
+++ resources/static/js/uplog-controller-charts.js
@@ -29,8 +29,111 @@ app.controller("ChartsController", function($scope) {
 
 		});
 
+		$scope.distributiondata = records.slice().map(function(record) {
+			return record.uptime/3600.;
+		});
+
 	});
 
+});
+
+app.directive('myDensityChart', function($parse) {
+	var margin = {top: 20, right: 20, bottom: 30, left: 50},
+		width = 960 - margin.left - margin.right,
+	   	height = 400 - margin.top - margin.bottom;
+
+     return {
+		restrict: 'E', // only as element
+		replace: false, // don't overwrite directive
+		scope: {data: '=chartData'},
+		link: function (scope, element, attrs) {
+			var data = scope.data;
+
+			if(data) {
+				draw(data, element[0]);
+			}
+
+			scope.$watch("data",function(newData) {
+				element.empty();
+				data = newData;
+				draw(data, element[0]);
+
+			});
+
+		} 
+	};
+
+	function draw(data, el) {
+		data.sort();
+
+		data = data.map(function(value, i) {
+			return {
+				x: value,
+				y: 1.*i/data.length,
+			};
+		});
+
+		var chart = d3.select(el);
+
+		var svg = chart.append("div").attr("class", "chart").append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g") .attr("transform", "translate(" + margin.left + ","
+					+ margin.top + ")");
+
+		var x = d3.scale.linear().range([0, width]);
+		var y = d3.scale.linear().range([height, 0]);
+
+		var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
+
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left");
+
+		var line = d3.svg.line()
+			.interpolate('step-after')
+    		.x(function(d) { return x(d.x); })
+		    .y(function(d) { return y(d.y); });
+
+		x.domain(d3.extent(data, function(d) { return d.x; }));
+		y.domain(d3.extent(data, function(d) { return d.y; }));
+
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis);
+
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis)
+
+		svg.append("clipPath")
+			.attr("id", "chart-area")
+			.append("rect")
+			.attr("x", 0)
+			.attr("y", 0)
+			.attr("width", width)
+			.attr("height", height);
+
+		svg.append("path")
+			.attr("clip-path", "url(#chart-area)")
+			.datum(data)
+			.attr("class", "line")
+			.attr('style', 'stroke: black;')
+			.attr("d", line);
+		svg.selectAll(".point")
+			.data(data)
+			.enter().append("circle")
+			//.attr("class", "point-"+set.key)
+			.attr("clip-path", "url(#chart-area)")
+			.attr("stroke", "black")
+			.attr("fill", function(d, i) { return "black" })
+			.attr("cx", function(d, i) { return x(d.x) })
+			.attr("cy", function(d, i) { return y(d.y) })
+			.attr("r", function(d, i) { return 1 });
+	}
 });
 
 
